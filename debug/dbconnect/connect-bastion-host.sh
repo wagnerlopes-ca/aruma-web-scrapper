512b1166790fb0d5e1c24448c51b2edf7a0cd8bb
wagnerlopes-ca <wagner@yourcareaccess.com.au>
2025-10-07 09:23:43 +1030

    Removing debug folder

--- debug/dbconnect/connect-bastion-host.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-ssh -i dev-bastion-key.pem -o StrictHostKeyChecking=no -T -N -L 0.0.0.0:3306:dev-database.cqlmrwfhitxc.ap-southeast-2.rds.amazonaws.com:3306 ubuntu@3.27.71.15
--- debug/docker/scripts/1_init-table.sql
+++
@@ -1,8 +0,0 @@
-USE careaccess;
-
-CREATE TABLE `OperationConfigs` (
-  `DeviceName` varchar(200) NOT NULL,
-  `DeviceFileName` varchar(200) DEFAULT NULL,
-  `StorageFileName` varchar(200) DEFAULT NULL,
-  `Contents` longtext
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
